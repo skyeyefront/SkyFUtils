21b485539511915b94e8bf58b6cb2cc80b53b9cb
j-huangwei-ty <j-huangwei-ty@360.cn>
2016-09-06 15:23:08 +0800

    动态生成Banner

--- src/apps/SimpleFunctionsDoc/Banner/index.js
+++ src/apps/SimpleFunctionsDoc/Banner/index.js
@@ -5,26 +5,66 @@ import style from './style.less'
 import template from './template.html'
 import CodePanel from '../../../components/CodePanel'
 import {LabelBrowser, LabelNode} from '../../../components/Label'
+import OptionsList from '../../../components/OptionsList'
 
 export default{
   domId: style.banner,
   labels: [ 'browser', 'node' ],
   template,
   data () {
+    console.log(window.SkyEyeBanner('skyeye', {notPrint: true}))
     return {
       style,
-      str: window.SkyEyeBanner(true)
+      str: window.SkyEyeBanner('skyeye', {notPrint: true}),
+      params: {
+        inputStr: 'skyeye',
+        scale: 1,
+        wordSpace: 4
+      },
+      paramsOptions: [
+        {
+          name: 'str',
+          type: 'String',
+          default: 'skyeye',
+          means: '待像素化的字符串',
+          required: false
+        },
+        {
+          name: 'scale',
+          type: 'Number',
+          default: '1',
+          means: '缩放倍数',
+          required: false
+        },
+        {
+          name: 'wordSpace',
+          type: 'Number',
+          default: '4',
+          means: '字符间距',
+          required: false
+        }
+      ]
+    }
+  },
+  computed: {
+    functionCodes: function () {
+      let str = 'SkyEyeBanner('
+      if (this.params.inputStr !== '') str += this.params.inputStr
+      if (this.params.scale !== '') str += ',' + this.params.scale
+      if (this.params.wordSpace !== '') str += ',' + this.params.wordSpace
+      str += ')'
+      return [str, '结果如下:', this.str]
     }
   },
   methods: {
     runIt () {
-      window.SkyEyeBanner()
+      this.str = window.SkyEyeBanner(this.params.inputStr, this.params.scale, this.params.wordSpace)
     }
   },
   components: {
-    CodePanel, LabelBrowser, LabelNode
+    CodePanel, LabelBrowser, LabelNode, OptionsList
   },
   ready () {
-    window.SkyEyeBanner()
+    window.SkyEyeBanner('skyeye')
   }
 }
--- src/libenv/usual/banner.js
+++ src/libenv/usual/banner.js
@@ -1,19 +1,92 @@
 /**
- * Created by huangxinxin on 16/9/2.
+ * Created by huangxinxin on  6/9/2.
  */
-export default function (notPrint) {
-  var arr = [
-    '******   *     *  *         *  ******  *         *  ******',
-    '*        *   *      *     *    *         *     *    *',
-    '*        * *          * *      *           * *      *',
-    '******   *             *       ******       *       ******',
-    '     *   * *           *       *            *       *',
-    '     *   *   *         *       *            *       *',
-    '******   *     *       *       ******       *       ******'
-  ]
-  let str = '\n' + arr.join('\n') + '\n'
-  if (notPrint) {
-    return str
+import bannerDict from './bannerDict'
+
+// 基元字母大小
+const baseSize = 7
+
+/**
+ * 返回输入字符串对应的像素化字符
+ * @param str 输入字符串
+ * @param args {scale: 缩放倍数, wordSpace: 字符间距, notPrint: 是否不打印}
+ * @returns {String}
+ */
+export default function (str, scale, wordSpace, notPrint) {
+  if (str === '') str = 'skyeye'
+  if (str === undefined || typeof str !== 'string') return 'error'
+
+  str = str.toUpperCase()
+
+  let _wordSpace = 4
+  let _scaleTimes = 1
+  let _notPrint = false
+  if (scale && parseInt(scale)) _scaleTimes = parseInt(scale)
+  if (wordSpace && parseInt(wordSpace)) _wordSpace = parseInt(wordSpace)
+  if (notPrint !== undefined) _notPrint = notPrint
+
+  let wordSpaceStr = ''
+  for (let i = 0; i < _wordSpace; i++) {
+    wordSpaceStr += ' '
   }
-  console.log(str)
+
+  let inputArr = str.split('')
+  let result = []
+  for (let i = 0; i < _scaleTimes * baseSize; i++) {
+    result[ i ] = ''
+  }
+  inputArr.forEach(function (character) {
+    let charArr = scaledLetter(bannerDict[ character ], _scaleTimes)
+    for (let i = 0; i < charArr.length; i++) {
+      result[ i ] += wordSpaceStr + charArr[ i ]
+    }
+  })
+
+  if (_notPrint) {
+    return getJointStr(result)
+  }
+  console.log(getJointStr(result))
+  return getJointStr(result)
 }
+
+/**
+ * 得到字母缩放scaleTimes倍数后的字母
+ * @param letterArr
+ * @param scaleTimes
+ * @returns {Array}
+ */
+function scaledLetter (letterArr, scaleTimes) {
+  let scaleSize = baseSize * scaleTimes
+  let twoDimArrBefore = []
+  letterArr.forEach(function (row) {
+    twoDimArrBefore.push(row.split(''))
+  })
+  let twoDimArrAfter = []
+  for (let i = 0; i < scaleSize; i++) {
+    twoDimArrAfter[ i ] = []
+    for (let j = 0; j < scaleSize; j++) {
+      twoDimArrAfter[ i ][ j ] = ' '
+    }
+  }
+  let result = []
+  for (let i = 0; i < baseSize; i++) {
+    for (let j = 0; j < baseSize; j++) {
+      twoDimArrAfter[ i * scaleTimes ][ j * scaleTimes ] = twoDimArrBefore[ i ][ j ]
+    }
+    result.push(twoDimArrAfter[ i * scaleTimes ].join(''))
+    for (let k = 1; k < scaleTimes; k++) {
+      result.push(twoDimArrAfter[ i * scaleTimes + scaleTimes - k ].join(''))
+    }
+  }
+  return result
+}
+
+/**
+ * 得到一位数组的拼接字符串
+ * @param arr
+ * @returns {string}
+ */
+function getJointStr (arr) {
+  return '\n' + arr.join('\n') + '\n'
+}
+
