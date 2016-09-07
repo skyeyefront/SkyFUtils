6f8ce27ee466fd3447dae1d129757e8a84277661
j-huangwei-ty <j-huangwei-ty@360.cn>
2016-09-07 16:00:51 +0800

    fileReader读取文件filter和onReadStart、onReadEnd

--- src/apps/SimpleFunctionsDoc/FileReader/index.js
+++ src/apps/SimpleFunctionsDoc/FileReader/index.js
@@ -6,7 +6,7 @@ import template from './template.html'
 import CodePanel from '../../../components/CodePanel'
 import {LabelBrowser, LabelNode} from '../../../components/Label'
 import OptionsList from '../../../components/OptionsList'
-const textMaxLength = 1000
+const textMaxLength = 500
 export default{
   domId: style.fileRead,
   labels: [ 'browser', 'file' ],
@@ -18,14 +18,24 @@ export default{
       readImgResult: '',
       readTextResult: '',
       readImageURL: '',
+      readAllImgURL: '',
+      filterImageURL: '',
+      readImageTips: {
+        text: 'ERROR: 您选择的文件不是图片类型,请选择图片文件!',
+        show: false
+      },
+      filterFun: '',
       fileTypeOptions: [ 'readAsArrayBuffer', 'readAsBinaryString', 'readAsDataURL', 'readAsText' ],
       params: {
         file: [],
         textFile: [],
         imgFile: [],
-        fileType: 'readAsText'
+        fileType: 'readAsText',
+        filterFile: '',
+        eventFile: '',
+        eventFileType: 'readAsText'
       },
-      paramsOptions: [{
+      paramsOptions: [ {
         name: 'file',
         type: 'File',
         default: 'null',
@@ -33,41 +43,81 @@ export default{
         required: true
       }, {
         name: 'fileType',
-        type: 'String',
+        type: '[ readAsArrayBuffer | readAsBinaryString | readAsDataURL | readAsText ]',
         default: 'readAsText',
-        means: '文件类型',
+        means: '以不同格式读取文件<br>' +
+        'readAsArrayBuffer: 读取文件为数组缓存格式<br>' +
+        'readAsBinaryString: 以二进制防止读取文件内容<br>' +
+        'readAsDataURL: 以图片方式读取文件内容<br>' +
+        'readAsText: 以文本格式读取文件内容<br>',
         required: false
-      }]
+      } ],
+      filterApiCodes: [ 'new SkyEyeFileRead(\'文件\')',
+        '.readAs(\'文件类型\')',
+        '.filter(function (f) {',
+        '  console.log(f)',
+        '  // 这里是filter方法，如过滤掉不是.txt的文件',
+        '  return f.name.split(\'.\').pop() === \'txt\'',
+        '})',
+        '.read()' ]
     }
   },
   computed: {
     functionCodes: function () {
       let str = ''
       if (this.params.file.length > 0) {
-        str = 'new SkyEyeFileRead(\'' + this.params.file[0].name + '\'' + ').readAs(\'' + this.params.fileType + '\').read()'
+        str = 'new SkyEyeFileRead(\'' + this.params.file[ 0 ].name + '\'' + ').readAs(\'' + this.params.fileType + '\').read()'
       } else {
         str = 'new SkyEyeFileRead().readAs().read()'
       }
       if (this.params.fileType === 'readAsDataURL') {
-        return [str, '结果如下:']
+        return [ str, '结果如下:' ]
       } else {
-        return [str, '结果如下:', this.str]
-      }
-    },
-    imageURL: function () {
-      if (this.params.fileType === 'readAsDataURL') {
-        return this.str
+        return [ str, '结果如下:', this.str ]
       }
     },
     readTextCodes () {
-      let str = 'new SkyEyeFileRead(' + (this.params.textFile.length > 0 ? '\'' + this.params.textFile[0].name + '\'' : '') + ')' +
+      let str = 'new SkyEyeFileRead(' + (this.params.textFile.length > 0 ? '\'' + this.params.textFile[ 0 ].name + '\'' : '') + ')' +
         '.readAs(\'readAsText\').read()'
-      return [str, '结果如下:', this.readTextResult]
+      return [ str, '结果如下:', this.readTextResult ]
     },
     readImgCodes () {
-      let str = 'new SkyEyeFileRead(' + (this.params.imgFile.length > 0 ? '\'' + this.params.imgFile[0].name + '\'' : '') + ')' +
-        '.readAs(\'readAsDataURL\').read()'
-      return [str, '结果如下:']
+      let tmp = [ 'new SkyEyeFileRead(' + (this.params.imgFile.length > 0 ? '\'' + this.params.imgFile[ 0 ].name + '\'' : '') + ')' +
+      '.readAs(\'readAsDataURL\').read()', '结果如下:' ]
+      if (this.readImageTips.show) tmp.push(this.readImageTips.text)
+      return tmp
+    },
+    filterCodes () {
+      return [ 'new SkyEyeFileRead(' + (this.params.filterFile.length > 0 ? '\'' + this.params.filterFile[ 0 ].name + '\'' : '') + ')',
+        '.readAs(\'readAsDataURL\')',
+        '.filter(function (f) {',
+        '  console.log(f)',
+        '  // 只通过图片格式文件',
+        '  return f.type.indexOf(\'image/\') !== -1',
+        '})',
+        '.read()', '结果如下:' ]
+    },
+    readEventCodes () {
+      return [ 'new SkyEyeFileRead(' + (this.params.eventFile.length > 0 ? '\'' + this.params.eventFile[ 0 ].name + '\'' : '') + ')',
+        '.readAs(\'' + this.params.eventFileType + '\')',
+        '.onReadStart(function (e, file) {',
+        '  console.log(\'============Read Start=================\')',
+        '  // 读取开始回调参数e为事件',
+        '  console.log(\'事件e\', e)',
+        '  // 读取开始回调参数file为待读取文件',
+        '  console.log(\'待读取文件\', file)',
+        '})',
+        '.onReadEnd(function (e, file, result) {',
+        '  console.log(\'============Read End=================\')',
+        '  // 读取结束回调参数e为事件',
+        '  console.log(\'事件e\', e)',
+        '  // 读取结束回调参数file为已读取的文件',
+        '  console.log(\'已读取文件file\', file)',
+        '  // 读取结束回调参数result为文件内容',
+        '  console.log(\'文件内容result\', result)',
+        '})',
+        '.read()'
+      ]
     }
   },
   methods: {
@@ -77,10 +127,11 @@ export default{
       fileReader
         .readAs(self.params.fileType)
         .onReadEnd(function (e, file, result) {
-          if (result.length > textMaxLength) {
-            self.str = result.substring(0, textMaxLength) + '...'
+          if (self.params.fileType === 'readAsDataURL') {
+            self.readAllImgURL = result
           } else {
-            self.str = result
+            self.readAllImgURL = ''
+            self.str = result.length > textMaxLength ? result.substring(0, textMaxLength) + '...' : result
           }
         })
         .read()
@@ -91,11 +142,7 @@ export default{
       fileReader
         .readAs('readAsText')
         .onReadEnd(function (e, file, result) {
-          if (result.length > textMaxLength) {
-            self.readTextResult = result.substring(0, textMaxLength) + '...'
-          } else {
-            self.readTextResult = result
-          }
+          self.readTextResult = result.length > textMaxLength ? result.substring(0, textMaxLength) + '...' : result
         })
         .read()
     },
@@ -105,7 +152,45 @@ export default{
       fileReader
         .readAs('readAsDataURL')
         .onReadEnd(function (e, file, result) {
-          self.readImageURL = result
+          if (file.type.indexOf('image/') === -1) {
+            self.readImageURL = ''
+            self.readImageTips.show = true
+          } else {
+            self.readImageURL = result
+            self.readImageTips.show = false
+          }
+        })
+        .read()
+    },
+    runFilter () {
+      let self = this
+      let fileReader = new window.SkyEyeFileRead(this.params.filterFile)
+      fileReader
+        .readAs('readAsDataURL')
+        .onReadEnd(function (e, file, result) {
+          self.filterImageURL = result
+        })
+        .filter(function (f) {
+          console.log(f)
+          return f.type.indexOf('image/') !== -1
+        })
+        .read()
+    },
+    runReadEvent () {
+      let self = this
+      let fileReader = new window.SkyEyeFileRead(this.params.eventFile)
+      fileReader
+        .readAs(self.params.eventFileType)
+        .onReadStart(function (e, file) {
+          console.log('============Read Start=================')
+          console.log('事件e', e)
+          console.log('待读取文件file', file)
+        })
+        .onReadEnd(function (e, file, result) {
+          console.log('============Read End=================')
+          console.log('事件e', e)
+          console.log('已读取文件file', file)
+          console.log('文件内容result', result.length > 200 ? result.substring(0, 200) : result)
         })
         .read()
     },
@@ -117,6 +202,12 @@ export default{
     },
     getImgFileName (e) {
       this.params.imgFile = e.target.files
+    },
+    getFilterFileName (e) {
+      this.params.filterFile = e.target.files
+    },
+    getEventFileName (e) {
+      this.params.eventFile = e.target.files
     }
   },
   components: {
